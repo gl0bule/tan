b2fbbc31d8a564b231263a672f6a083cfd0365f3
gl0bule <j.zrk369@gmail.com>
2017-11-26 18:19:19 +0100

    Création du service stop

--- src/app/services/stop.service.ts
+++ src/app/services/stop.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class StopService {
 
-  constructor() { }
-
+  constructor(private http: HttpClient) { }
+  public getStops(): Observable<String> {
+    return this.http.get("http://localhost:4200/ewp/arrets.json").map(o => JSON.stringify(o));
+  }
 }
--- src/app/stop-list/stop-list.component.ts
+++ src/app/stop-list/stop-list.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { StopService} from '../services/stop.service';
 
 @Component({
   selector: 'app-stop-list',
   templateUrl: './stop-list.component.html',
-  styleUrls: ['./stop-list.component.css']
+  styleUrls: ['./stop-list.component.css'],
+  providers: [StopService]
 })
 export class StopListComponent implements OnInit {
 longitude: number = -1.553718;
 latitude: number = 47.21496;
-
-  constructor() { }
+val: String = "";
+stops: Array<string> = new Array();
+  constructor(private stopService: StopService) { }
 
   ngOnInit() {
+    this.stops.push("commerce");
+    this.stops.push("ecole");
+    this.stops.push("stade");
+    this.stops.push("mairie");
   }
 
 }
